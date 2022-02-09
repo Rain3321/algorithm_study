5500d01ec1b847ba3b8e1104cdc2b7bada8998f7
minwoo9629 <minwoo9629@gmail.com>
2022-02-09 23:28:51 +0900

    [백준] 2015. 수들의 합 4 문제풀이(수정)

--- minwoo.lee_java/src/BOJ2015.java
+++ minwoo.lee_java/src/BOJ2015.java
@@ -6,31 +6,28 @@ public class BOJ2015 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         HashMap<Integer, Integer> numSums = new HashMap<>();
-        int n, k;
-        long result;
-        int[] nums;
-        int[] prefix;
-        result = 0L;
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        nums = new int[n + 1];
-        prefix = new int[n + 1];
+        long result = 0L;
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] nums = new int[n + 1];
         nums[0] = 0;
-        prefix[0] = 0;
+
         st = new StringTokenizer(bf.readLine());
         for (int i = 1; i <= n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
+        int[] prefixSum = new int[n + 1];
+        prefixSum[0] = 0;
         for (int i = 1; i <= n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
 
         numSums.put(0, 1);
         for (int i = 1; i <= n; i++) {
-            result += (long)numSums.getOrDefault(prefix[i] - k, 0);
-            int temp = numSums.getOrDefault(prefix[i], 0) + 1;
-            numSums.put(prefix[i], temp);
+            result += (long) numSums.getOrDefault(prefixSum[i] - k, 0);
+            int temp = numSums.getOrDefault(prefixSum[i], 0) + 1;
+            numSums.put(prefixSum[i], temp);
         }
         bf.close();
         System.out.println(result);
