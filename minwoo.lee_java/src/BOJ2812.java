ba970a1b58e58c46cef4c3475d297c22ebc1dedb
minwoolee <46440898+minwoo9629@users.noreply.github.com>
2022-02-21 22:16:33 +0900

    [백준] 2812. 크게 만들기 문제풀이 (#136)
    * [백준] 2812. 크게 만들기 문제풀이
    
    * [백준] 2812. 크게 만들기 문제풀이(수정)

---
+++ minwoo.lee_java/src/BOJ2812.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ2812 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int printCnt = N - k;
+        char[] nums = br.readLine().toCharArray();
+        Stack<Character> stack = new Stack<>();
+        stack.push(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            while (!stack.isEmpty() && k != 0 && stack.peek() < nums[i]) {
+                stack.pop();
+                k--;
+            }
+            stack.push(nums[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < printCnt; i++) {
+            sb.append(stack.get(i));
+        }
+        System.out.println(sb);
+    }
+}
