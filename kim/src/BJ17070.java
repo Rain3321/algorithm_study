59ae319281d08ed9100ad34daa2607efc3b6b778
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-02-24 01:49:27 +0900

    [백준]17070, 파이프 옮기기 1 문제풀이

--- kim/src/BJ17070.java
+++ kim/src/BJ17070.java
@@ -28,6 +28,7 @@ public class BJ17070 {
                     dp[i + 1][j][2] += dp[i][j][1] + dp[i][j][2];
             }
         }
+
         System.out.println(dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2]);
     }
 }
