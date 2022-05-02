210ea5779b505f66998cd96b222ccb1ad4c67887
minwoo9629 <minwoo9629@gmail.com>
2022-05-02 23:40:44 +0900

    [PG] 경주로 건설 문제풀이(수정)

---
+++ minwoo.lee_java/src/BOJ2696.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ2696 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int M = Integer.parseInt(br.readLine());
+            for (int i = 0; i <= M / 11; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M / 10 * 1 + M % 10; j++) {
+
+                }
+            }
+        }
+    }
+}
